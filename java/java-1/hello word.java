16f4fc7a7510b6f0d9f7dd3fe76d75539bacdb42
小五 <wu.vkcyan@gmail.com>
2018-06-12 00:26:34 +0800

    转行了!学习java了!

---
+++ java/java-1/hello word.java
@@ -0,0 +1,17 @@
+﻿class demo
+{
+    public static void main(String[] args){
+      char key = '1';
+      switch (key) {
+        case '1':
+          System.out.println("星期一");
+          break;
+        case '2':
+          System.out.println("星期二");
+          break;
+        default:
+          System.out.println("不行");
+          break;
+      }
+    }
+}
